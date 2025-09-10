f059c3d1eed0a161d13c1ebcdfce5015d1f8b55d
bodbal <b1bodbal@vasvari.org>
2025-09-10 11:27:02 +0200

    leált az agyam

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 function Switch () {
-const val = document.getElementById("val").value;
+document.querySelectorAll('input[name="switch"]').forEach(radio => {
+    radio.addEventListener('change', function() {
+        if (this.value === 'option1') {
+            SignUp();
+        } else if (this.value === 'option2') {
+            Login();
+        }
+    });
 
-}
+});
+};
 
 function SignUp() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     const password2 = document.getElementById("password2").value;
+    const signupDiv = document.getElementById("signupDiv");
+    
     if (email === "" || password === "" || password2 === "") {
         alert("Kérem, töltse ki az összes mezőt!");
         return;
@@ -15,11 +25,11 @@ function SignUp() {
         alert("A jelszavak nem egyeznek!");
         return;
     }
-    
 
-
-
-
+    if (signupDiv){
+        //signupDiv.innerHTML = " Sign Up ";
+        alert("jóóóóóó")
+    }
 
 alert("Sikeres regisztráció!");
 }
@@ -31,6 +41,8 @@ function Login() {
         alert("Kérem, töltse ki az összes mezőt!");
         return;
     }
+    
+
 
    alert("Sikeres bejelentkezés!");
 
