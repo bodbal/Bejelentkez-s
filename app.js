6bad132404adad05481f8582e94af9514e73680a
bodbal <b1bodbal@vasvari.org>
2025-09-12 10:24:16 +0200

    ablakok megjelenítése

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-function Switch () {
-document.querySelectorAll('input[name="switch"]').forEach(radio => {
-    radio.addEventListener('change', function() {
-        if (this.value === 'option1') {
-            SignUp();
-        } else if (this.value === 'option2') {
-            Login();
-        }
-    });
-
-});
-};
-
+function showDiv(divId) {
+    document.getElementById("LoginDiv").style.display = "none";
+    document.getElementById("SignUpDiv").style.display = "none";
+    document.getElementById(divId).style.display = "block";
+  }
 function SignUp() {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const password2 = document.getElementById("password2").value;
-    const signupDiv = document.getElementById("signupDiv");
+    const email = document.getElementById("reg-email").value;
+    const password = document.getElementById("reg-password").value;
+    const password2 = document.getElementById("reg-password2").value;
+    const signupDiv = document.getElementById("SignUpDiv");
     
     if (email === "" || password === "" || password2 === "") {
         alert("Kérem, töltse ki az összes mezőt!");
@@ -35,15 +27,21 @@ alert("Sikeres regisztráció!");
 }
 
 function Login() {
+
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
+    const loginDiv = document.getElementById("LoginDiv");
     if (email === "" || password === "") {
         alert("Kérem, töltse ki az összes mezőt!");
         return;
     }
+
     
 
 
    alert("Sikeres bejelentkezés!");
 
 }
+window.onload = function () {
+    showDiv("LoginDiv");
+  };
